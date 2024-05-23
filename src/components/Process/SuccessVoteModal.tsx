d156beb04170ef19783e42793aeb6e7948889db2
Òscar Casajuana <elboletaire@gmail.com>
2024-05-23 18:41:50 +0200

    Move process modals to their own components

---
+++ src/components/Process/SuccessVoteModal.tsx
@@ -0,0 +1,92 @@
+import {
+  Box,
+  Button,
+  Link,
+  ListItem,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  UnorderedList,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { environment } from '@vocdoni/chakra-components'
+import { useClient, useElection } from '@vocdoni/react-providers'
+import { InvalidElection } from '@vocdoni/sdk'
+import { useEffect, useState } from 'react'
+import { Trans, useTranslation } from 'react-i18next'
+import { FacebookShare, RedditShare, TelegramShare, TwitterShare } from '~components/Share'
+import successImg from '/assets/spreadsheet-success-modal.jpg'
+
+export const SuccessVoteModal = () => {
+  const { t } = useTranslation()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { votesLeft, election, voted } = useElection()
+  const { env } = useClient()
+
+  const [vLeft, setVLeft] = useState<number>(0)
+
+  useEffect(() => {
+    if (!vLeft && votesLeft >= 0) {
+      setVLeft(votesLeft)
+    }
+
+    if (vLeft && votesLeft < vLeft) {
+      setVLeft(votesLeft)
+      onOpen()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [votesLeft, vLeft])
+
+  if (!election || !voted || election instanceof InvalidElection) return null
+
+  const verify = environment.verifyVote(env, voted)
+  const url = encodeURIComponent(document.location.href)
+  const caption = t('process.share_caption', { title: election?.title.default })
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>
+          <Text>{t('process.success_modal.title')}</Text>
+          <Box bgImage={successImg} />
+        </ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Trans
+            i18nKey='process.success_modal.text'
+            components={{
+              verify: <Link href={verify} target='_blank' />,
+              p: <Text mb={2} />,
+            }}
+          />
+          <UnorderedList listStyleType='none' display='flex' justifyContent='center' gap={6} mt={6} mb={2} ml={0}>
+            <ListItem>
+              <TwitterShare url={url} caption={caption} />
+            </ListItem>
+            <ListItem>
+              <FacebookShare url={url} caption={caption} />
+            </ListItem>
+            <ListItem>
+              <TelegramShare url={url} caption={caption} />
+            </ListItem>
+            <ListItem>
+              <RedditShare url={url} caption={caption} />
+            </ListItem>
+          </UnorderedList>
+        </ModalBody>
+
+        <ModalFooter mt={4}>
+          <Button onClick={onClose} variant='primary'>
+            {t('process.success_modal.btn')}
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
