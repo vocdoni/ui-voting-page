de903dcbfd91b88c20202719e0779aa96460ff9e
emmdim <emmdim@users.noreply.github.com>
2024-05-24 15:11:35 +0200

    WIP PrivKey url login for addresses census

--- src/components/Process/View.tsx
+++ src/components/Process/View.tsx
@@ -1,15 +1,53 @@
 import { Box, Button, Flex, Image } from '@chakra-ui/react'
 import { QuestionsFormProvider, SpreadsheetAccess } from '@vocdoni/chakra-components'
-import { useElection } from '@vocdoni/react-providers'
+import { useElection, useClient } from '@vocdoni/react-providers'
+import { useEffect, useRef } from 'react'
+import { useTranslation } from 'react-i18next'
 import { VocdoniAppURL } from '~constants'
 import { ConfirmVoteModal } from './ConfirmVoteModal'
 import Header from './Header'
 import { Questions } from './Questions'
 import { SuccessVoteModal } from './SuccessVoteModal'
 import omniumLogoHeader from '/assets/omnium-logo.png'
+import { ArchivedElection, VocdoniSDKClient } from '@vocdoni/sdk'
+import { Wallet } from '@ethersproject/wallet'
 
 export const ProcessView = () => {
-  const { connected } = useElection()
+  const { t } = useTranslation()
+  const { isAbleToVote, connected, election, client: electionClient, setClient } = useElection()
+  const { env } = useClient()
+  const electionRef = useRef<HTMLDivElement>(null)
+
+  const shouldRender = !(election instanceof ArchivedElection)
+
+  const privkey = window.location.hash ? window.location.hash.split('#')[1] : ''
+
+  // In case of spreadsheet census and a private provided through the URI, do intent to login automatically
+  // Example url with private key:
+  // https://vote.omnium.cat/0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef#0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef
+  useEffect(() => {
+    ;(async () => {
+      try {
+        if (!shouldRender || connected) return
+        if (privkey) {
+          const privKeyWallet = new Wallet(privkey)
+          let client = new VocdoniSDKClient({
+            env,
+            wallet: privKeyWallet,
+            electionId: election?.id,
+          })
+          let electionClientAddress =(electionClient.wallet && electionClient.wallet instanceof Wallet) ? await electionClient.wallet?.getAddress(): null
+          let walletAddress = await client.wallet?.getAddress()
+          if (!!!electionClient.wallet || electionClientAddress !== walletAddress) {
+            setClient(client)
+          }
+        }
+      } catch (error) {
+        console.warn('Error trying to login with private key ', error)
+        // setClient(electionClient)
+      }
+    })()
+  }, [election, env, shouldRender, privkey])
 
   return (
     <>
@@ -28,7 +66,7 @@ export const ProcessView = () => {
           </Button>
         </a>
 
-        {connected && (
+        {connected && !privkey.length && (
           <Box>
             <SpreadsheetAccess />
           </Box>
