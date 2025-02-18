53a3bc47a9a7b7a1d1b7b68c66a6d7a7f3bfaa2d
Òscar Casajuana <elboletaire@gmail.com>
2025-02-18 16:15:47 +0100

    fix(pxll): final text should only be visible for non-logged-in users

--- src/elements/Home/Pxll.tsx
+++ src/elements/Home/Pxll.tsx
@@ -53,12 +53,15 @@ const Pxll = () => {
         </UnorderedList>
       </Box>
       <SpreadsheetAccess hashPrivateKey />
-      {connected && import.meta.env.PROCESS_IDS.map((id: string) => <ElectionButtonCard id={id} key={id} />)}
-      <Text>
-        Per poder accedir a la votació i veure el vídeo en temps real, heu de prémer sobre “Identificar-se”. Us
-        demanarem el vostre DNI/NIE i el codi de pas que heu rebut per correu electrònic. Posteriorment, podreu emetre
-        el vostre vot de forma segura.
-      </Text>
+      {connected ? (
+        import.meta.env.PROCESS_IDS.map((id: string) => <ElectionButtonCard id={id} key={id} />)
+      ) : (
+        <Text>
+          Per poder accedir a la votació i veure el vídeo en temps real, heu de prémer sobre “Identificar-se”. Us
+          demanarem el vostre DNI/NIE i el codi de pas que heu rebut per correu electrònic. Posteriorment, podreu emetre
+          el vostre vot de forma segura.
+        </Text>
+      )}
     </Flex>
   )
 }
