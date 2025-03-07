1f9996d513ddfef8db0a4f45c0eea8c5eb699adf
Gerard <g.roura.v@protonmail.com>
2025-03-07 14:37:03 +0100

    removed logo condition

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -9,11 +9,9 @@ const Layout = () => {
   return (
     <Grid p={3} minH='100vh'>
       <Stack direction='row' mb={3} w='full' justifyContent='space-between'>
-        {import.meta.env.CLIENT !== 'default' && (
-          <Link as={RouterLink} to={`/${window.location.hash}`}>
-            <Image src={`/assets/${import.meta.env.CLIENT.toLowerCase()}/logo.png`} alt='logo' maxW='250px' />
-          </Link>
-        )}
+        <Link as={RouterLink} to={`/${window.location.hash}`}>
+          <Image src={`/assets/pxll/logo.png`} alt='logo' maxW='250px' />
+        </Link>
         <Stack direction='row' alignItems='center' mb={3} alignSelf='start' ml='auto'>
           <Button as={Link} variant='link' isExternal href={VocdoniAppURL}>
             Admin
