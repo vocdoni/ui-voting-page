8d48b659baf1dcd91f92f1b5f341b379fb749d45
emmdim <emmdim@users.noreply.github.com>
2026-04-07 19:23:38 +0200

    Stub components and sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import { ClientProvider } from '@vocdoni/chakra-components'
 import { EnvOptions } from '@vocdoni/sdk'
-import { Signer } from 'ethers'
 import { useTranslation } from 'react-i18next'
-import { useWalletClient } from 'wagmi'
 import { VocdoniEnvironment } from '~constants'
 import { translations } from '~i18n/components'
-import { clientToSigner } from '~util/wagmi-adapters'
 import { RoutesProvider } from './router'
 
 export const App = () => {
